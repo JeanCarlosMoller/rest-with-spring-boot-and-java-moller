154fc1e816be648767e22b5cf2564894fd7075e2
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-12-26 22:56:08 -0300

    Dando continuidade com os estudos.

--- rest-with-spring-boot-and-java-moller/rest-with-spring-boot-and-java-moller/src/main/java/br/com/moller/restwithspringbootandjavamoller/services/PersonServices.java
+++ rest-with-spring-boot-and-java-moller/rest-with-spring-boot-and-java-moller/src/main/java/br/com/moller/restwithspringbootandjavamoller/services/PersonServices.java
@@ -1,5 +1,7 @@
 package br.com.moller.restwithspringbootandjavamoller.services;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,18 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public List<Person> findAll() {
+
+        logger.info("Finding all people!");
+
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = MockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
     public Person findById(String id) {
 
         logger.info("Finding one person!");
@@ -22,6 +36,34 @@ public class PersonServices {
         person.setFirstName("Jean Carlos");
         person.setLastName("Möller");
         person.setAddress("Blumenau - SC");
+        person.setGender("Male");
+        return person;
+    }
+
+    public Person create(Person person) {
+
+        logger.info("Creating one person!");
+        return person;
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Updating one person!");
+        return person;
+    }
+
+    public void delete(String id) {
+
+        logger.info("Deleting one person!");
+    }
+
+    private Person MockPerson(int i) {
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
         person.setGender("Male");
         return person;
     }
