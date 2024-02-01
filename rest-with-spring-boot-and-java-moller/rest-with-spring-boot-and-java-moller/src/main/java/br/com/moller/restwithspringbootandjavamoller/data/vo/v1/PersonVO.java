d4f0a2fa9cdb03a45d2390ea098a01cc952e4954
Jean Carlos Möller <jeancmoller17@gmail.com>
2024-02-01 20:25:01 -0300

    Custom JSON Serialization

--- rest-with-spring-boot-and-java-moller/rest-with-spring-boot-and-java-moller/src/main/java/br/com/moller/restwithspringbootandjavamoller/data/vo/v1/PersonVO.java
+++ rest-with-spring-boot-and-java-moller/rest-with-spring-boot-and-java-moller/src/main/java/br/com/moller/restwithspringbootandjavamoller/data/vo/v1/PersonVO.java
@@ -1,17 +1,27 @@
 package br.com.moller.restwithspringbootandjavamoller.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 
+@JsonPropertyOrder({"id", "gender", "first_Name", "last_Name", "address"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private long id;
+
+    @JsonProperty("first_Name")
     private String firstName;
+
+    @JsonProperty("last_Name")
     private String lastName;
     private String address;
+
+    @JsonIgnore
     private String gender;
 
     public PersonVO() {
